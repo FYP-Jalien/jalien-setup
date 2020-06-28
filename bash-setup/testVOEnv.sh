3ce5898a7389ea918e5d3d8d90955e0083db94c8
drholmie <animesh.leo@gmail.com>
2020-06-28 23:21:35 +0530

    Adding feedback for CreateLDAP.sh - Reduced number of variables - Simplified ldap operations via file templates - Current paths and file names hardcoded

--- bash-setup/testVOEnv.sh
+++ bash-setup/testVOEnv.sh
@@ -3,6 +3,7 @@
 #for CreateCertificates.sh needs two folders: globus and trusts
 [[ -z $custom_jalienpath ]] && new_jalienpath=$HOME || new_jalienpath=$custom_jalienpath
 
+#certificate paths
 TVO_CERTS="${new_jalienpath}/.j/testVO/globus"
 TVO_TRUSTS="${new_jalienpath}/.j/testVO/trusts"
 CERTSUBJECT_CA="/C=CH/O=JAliEn/CN=JAliEnCA"
@@ -10,3 +11,9 @@ CERTSUBJECT_USER="/C=CH/O=JAliEn/CN=jalien"
 CERTSUBJECT_HOST="/C=CH/O=JAliEn/CN=localhost.localdomain"
 CERTSUBJECT_JAUTH="/C=CH/O=JAliEn/CN=jAuth"
 CERTSUBJECT_SE="/C=CH/O=JAliEn/CN=TESTSE"
+
+#ldap
+ldap_config="${new_jalienpath}/.j/testVO/config/ldap.config"
+ldap_home="${new_jalienpath}/.j/testVO/slapd"
+tvo_bin="${new_jalienpath}/.j/testVO/bin"
+ldap_log="${new_jalienpath}/.j/testVO/logs/ldap.log"
