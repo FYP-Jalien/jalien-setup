9bd4c45fd7a1bbee341c85a3b780110931561e19
drholmie <animesh.leo@gmail.com>
2020-08-10 18:19:33 +0530

    Fixed XRootD installation in JAliEn replica

--- dev/setup.sh
+++ dev/setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 git clone --depth=1 https://gitlab.cern.ch/jalien/jalien.git /jalien
-git clone --depth=1 https://github.com/xrootd/xrootd.git --branch=stable-4.12.x
+apt install -y wget
+wget https://xrootd.slac.stanford.edu/download/v4.12.1/xrootd-4.12.1.tar.gz
+tar xvzf xrootd-4.12.1.tar.gz
 mkdir /build && cd /build
-cmake /xrootd -DCMAKE_INSTALL_PREFIX=/opt/xrootd -DENABLE_PERL=FALSE
+cmake /xrootd-4.12.1 -DCMAKE_INSTALL_PREFIX=/usr -DENABLE_PERL=FALSE
 make && make install
-cp /opt/xrootd/lib/* /lib/
 cd /
