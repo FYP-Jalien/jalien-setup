8a0cb4c47c914f8a3bcd471382984bda1a7dd613
drholmie <animesh.leo@gmail.com>
2020-08-16 17:04:51 +0530

    Added CVMFS checker to check if CVMFS works and install required dependencies

---
+++ ce-setup/cvmfs-checker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+yum install -y environment-modules tcl
+if [[ -d /cvmfs/alice.cern.ch/bin]] then {
+    bash /start.sh &
+    exit
+}
+else {
+    #add appropriate symlinks to allow functioning of agent startup script
+
+    #start htcondor
+    bash /start.sh
+}
+fi
