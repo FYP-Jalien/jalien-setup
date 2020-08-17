5c9bc0a43b55064684efbfc2e56456e9b0ed209b
drholmie <animesh.leo@gmail.com>
2020-08-17 12:42:33 +0530

    Changed back to alien-cs.jar from alien-users.jar

--- bash-setup/entrypoint.sh
+++ bash-setup/entrypoint.sh
@@ -15,10 +15,10 @@ tail --pid $! -f $LOGS/setup_log.txt
 CreateDB.sh $target/sql &>>$LOGS/setup_log.txt &
 tail --pid $! -f $LOGS/setup_log.txt
 
-JCENTRAL_CMD="java -cp $JALIEN_DEV/alien-users.jar -Duserid=$(id -u) -DAliEnConfig=/jalien-dev/config/JCentral alien.JCentral $(pwd)"
+JCENTRAL_CMD="java -cp $JALIEN_DEV/alien-cs.jar -Duserid=$(id -u) -DAliEnConfig=/jalien-dev/config/JCentral alien.JCentral $(pwd)"
 
 #running optimizer
-bash optimisers.sh &> /dev/null &
+bash optimiser.sh &> /dev/null &
 
 ls $JALIEN_DEV/*.jar | entr -rcs "$JCENTRAL_CMD &>$LOGS/jcentral_stdout.txt" &
 tail --pid $! -f $LOGS/jcentral_stdout.txt
