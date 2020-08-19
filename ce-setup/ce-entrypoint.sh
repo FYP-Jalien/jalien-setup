4b6c02ed3d7aa9184515535eb8cb6dcdf736204a
drholmie <animesh.leo@gmail.com>
2020-08-19 20:31:11 +0530

    Changed paths of jar and config to shared volume for `ce-entrypoint.sh`

--- ce-setup/ce-entrypoint.sh
+++ ce-setup/ce-entrypoint.sh
@@ -5,10 +5,6 @@ JALIEN_DEV=/jalien-dev
 CE_CONFIG=$JALIEN_DEV/config/ComputingElement/docker
 LOGS=$JALIEN_DEV/logs
 HTCONDOR_CONF=/ce-setup/htcondor-conf
-#isntall dependecies for cvmfs
-export DEBIAN_FRONTEND=noninteractive
-export LC_ALL=C
-apt install -y environment-modules tcl
 #setup htcondor conf and start it
 cp $HTCONDOR_CONF/01* /etc/condor/config.d
 cp $HTCONDOR_CONF/start.sh $HTCONDOR_CONF/update-secrets $HTCONDOR_CONF/update-config / 
@@ -21,14 +17,12 @@ echo "submituser:toor" | chpasswd
 cp $CE_CONFIG/custom-classad.jdl /home/submituser
 su submituser -c "mkdir /home/submituser/tmp /home/submituser/log"
 touch /home/submituser/no-proxy-check /home/submituser/enable-sandbox
-cp $JALIEN_DEV/alien-cs.jar /home/submituser/
-cp -r $CE_CONFIG /home/submituser
 
 #run CE with auto reloading
-CE_CMD="java -cp /home/submituser/alien-cs.jar -Duserid=$(id -u) -Dcom.sun.jndi.ldap.connect.pool=false -DAliEnConfig=/home/submituser/docker -Djava.net.preferIPv4Stack=true alien.site.ComputingElement"
+CE_CMD="java -cp $JALIEN_DEV/alien-cs.jar -Duserid=$(id -u) -Dcom.sun.jndi.ldap.connect.pool=false -DAliEnConfig=$CE_CONFIG -Djava.net.preferIPv4Stack=true alien.site.ComputingElement"
 #CE_CMD="java -cp $JALIEN_DEV/alien-users.jar -server -XX:+OptimizeStringConcat -XX:CompileThreshold=20000 -Xms64m -Xmx512m  -XX:+UseG1GC -XX:+DisableExplicitGC -XX:+UseCompressedOops -XX:MaxTrivialSize=1K -Duserid=$(id -u) -Dcom.sun.jndi.ldap.connect.pool=false --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED --add-opens=java.rmi/sun.rmi.transport=ALL-UNNAMED -Djava.io.tmpdir=/tmp -DAliEnConfig=$CE_CONFIG -Djava.net.preferIPv4Stack=true alien.site.ComputingElement"
 
 #wait for JCentral-dev
 while ! /cvmfs/alice.cern.ch/bin/alienv setenv xjalienfs -c ". ${JALIEN_DEV}/env_setup.sh && alien.py pwd"; do sleep 1; done
 
-ls /home/submituser/*.jar | entr -rcs "su submituser -c \"$CE_CMD\""
+ls $JALIEN_DEV/*.jar | entr -rcs "su submituser -c \"$CE_CMD\""
