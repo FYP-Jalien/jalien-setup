fdb0b1bf7d3c9d99e6ee8611aedfe700b532463f
Nikola Hardi <nhardi@cern.ch>
2020-12-17 14:19:13 +0100

    Remove redundant line in xrootd image build script

--- xrootd/installer.sh
+++ xrootd/installer.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-yum -y install python3 cmake3 python3-devel
-echo -e "\n\n\n\npython done"
 yum -y install curl.x86_64 bind-utils.x86_64 redhat-lsb.x86_64 bzip2.x86_64 https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 rpm --import http://linuxsoft.cern.ch/wlcg/RPM-GPG-KEY-wlcg
 echo -e "\n\n\nrpm done"
@@ -10,6 +8,7 @@ yum -y install alicexrdplugins xrootd xrootd-server xrootd-client xrootd-client-
 echo -e "\n\n\n alice + xrootd done"
 yum -y install python3 cmake3 python3-devel
 pip3 install wheel setuptools
+echo -e "\n\n\n\npython done"
 pip3 install alienpy
 ln -s /usr/bin/cmake3 /usr/bin/cmake
 mkdir /shared-volume
