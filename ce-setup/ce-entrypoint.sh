e73b8c49bf5dbe62b2e66ce8f7e557bf62def238
Elena Mihailescu <maria.mihailescu@upb.ro>
2023-11-22 11:47:19 +0200

    Copy CERT-GridCA.pem in $X509_CERT_DIR
    If `CERT-GridCA.pem` is not found in `$X509_CERT_DIR`,
    `alien.py` ignores it.
    
    Signed-off-by: Elena Mihailescu <maria.mihailescu@upb.ro>

--- ce-setup/ce-entrypoint.sh
+++ ce-setup/ce-entrypoint.sh
@@ -18,6 +18,9 @@ cp $CE_CONFIG/custom-classad.jdl /home/submituser
 [ ! -e /home/submituser/tmp ] && su submituser -c "mkdir /home/submituser/tmp /home/submituser/log"
 touch /home/submituser/no-proxy-check /home/submituser/enable-sandbox
 
+# Copy CERT-GridCA.pem to globus/CA
+cp /cvmfs/alice.cern.ch/etc/grid-security/certificates/CERN-GridCA.pem $JALIEN_DEV/globus/CA/
+
 #run CE with auto reloading
 CE_CMD="java -cp $JALIEN_DEV/alien-cs.jar -Duserid=$(id -u) -Dcom.sun.jndi.ldap.connect.pool=false -DAliEnConfig=$CE_CONFIG -Djava.net.preferIPv4Stack=true alien.site.ComputingElement"
 #CE_CMD="java -cp $JALIEN_DEV/alien-users.jar -server -XX:+OptimizeStringConcat -XX:CompileThreshold=20000 -Xms64m -Xmx512m  -XX:+UseG1GC -XX:+DisableExplicitGC -XX:+UseCompressedOops -XX:MaxTrivialSize=1K -Duserid=$(id -u) -Dcom.sun.jndi.ldap.connect.pool=false --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED --add-opens=java.rmi/sun.rmi.transport=ALL-UNNAMED -Djava.io.tmpdir=/tmp -DAliEnConfig=$CE_CONFIG -Djava.net.preferIPv4Stack=true alien.site.ComputingElement"
