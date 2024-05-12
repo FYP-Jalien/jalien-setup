ef50fd1047fbba99620204df4c2962ac1c51d096
jananga99 <jananga1999@gmail.com>
2024-05-12 22:02:44 +0530

    a

--- ce-setup/docker-setup.sh
+++ ce-setup/docker-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+
 yum update -y
 yum install -y wget
 wget --no-check-certificate https://cdn.azul.com/zulu/bin/zulu11.68.17-ca-jdk11.0.21-linux.x86_64.rpm && yum -y install zulu11.68.17-ca-jdk11.0.21-linux.x86_64.rpm && rm zulu11.68.17-ca-jdk11.0.21-linux.x86_64.rpm
